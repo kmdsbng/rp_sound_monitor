9583f818add0663c9370de30d09d93caf04aad5a
Yoshihiro Kameda <kameda.sbng@gmail.com>
2020-02-11 09:42:54 +0900

    refactoring

--- sound_monitor.py
+++ sound_monitor.py
@@ -55,121 +55,140 @@ carryOn = True
 
 clock = pygame.time.Clock()
 
-sound_logs =[]
 font = pygame.font.Font(None, 60)
 small_font = pygame.font.Font(None, 20)
-alert_count = 0
-alert_count_date = datetime.date.today()
-
-while carryOn:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            carryOn = False
-    if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-        pygame.display.set_mode(size)
-    if event.type == pygame.KEYDOWN and event.key == pygame.K_f:
-        pygame.display.set_mode(size, FULLSCREEN)
-
-    now = datetime.datetime.now()
-    in_night = is_night_time(now)
-
-    # Game logic
-    #pygame.draw.rect(screen, RED, [55, 200, 100, 70], 0)
-    #pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-    #pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
-  
-    sound_logs_len = len(sound_logs)
-    back_ground_color = BLACK
-    max_sound = 0
-    for sound in sound_logs:
-        if max_sound < sound:
-            max_sound = sound
-
-    text_color = WHITE
-    if max_sound > ALERT_THRESHOLD:
-        back_ground_color = BACK_GROUND_RED
-
-    if alert_count > 0:
-        text_color = TEXT_RED
 
 
-    screen.fill(back_ground_color)
-    pygame.draw.line(screen, RED, [0, 400 - ALERT_THRESHOLD / 2], [600, 400 - ALERT_THRESHOLD / 2], 1)
-    pygame.draw.line(screen, GRAY, [0, 401], [600, 401], 1)
+class ScreenRenderer:
+    def __init__(self, screen, back_ground_color, alert_count, text_color, in_night, sound_logs):
+        self.screen = screen
+        self.back_ground_color = back_ground_color
+        self.alert_count = alert_count
+        self.text_color = text_color
+        self.in_night = in_night
+        self.sound_logs = sound_logs
 
-    #alert_count_text = font.render("ALERT", True, text_color)
-    #screen.blit(alert_count_text, [605, 100])
-    alert_count_text = font.render("%d" % (alert_count), True, text_color)
-    screen.blit(alert_count_text, [605, 100])
+    def render(self):
+        self.screen.fill(self.back_ground_color)
 
-    if in_night:
-        night_str = "n"
-    else:
-        night_str = "d"
+        pygame.draw.line(self.screen, RED, [0, 400 - ALERT_THRESHOLD / 2], [600, 400 - ALERT_THRESHOLD / 2], 1)
+        pygame.draw.line(self.screen, GRAY, [0, 401], [600, 401], 1)
 
-    night_text = small_font.render(night_str, True, text_color)
-    screen.blit(night_text, [700, 450])
+        alert_count_text = font.render("%d" % (self.alert_count), True, self.text_color)
+        screen.blit(alert_count_text, [605, 100])
 
-    for index, sound in enumerate(sound_logs):
-        graph_color = GREEN
-        width = 3
-        if sound > ALERT_THRESHOLD:
-            graph_color = WHITE
-            width = 30
-        elif sound > 200:
-            graph_color = YELLOW
-            width = 10
-        elif sound > 100:
-            graph_color = CYAN
-            width = 5
+        if self.in_night:
+            night_str = "n"
+        else:
+            night_str = "d"
 
-        x = (sound_logs_len - index) * 3
-        pygame.draw.line(screen, graph_color, [x, 400], [x, 400 - sound / 2], width)
-  
-    pygame.display.flip()
-  
-    inputs = []
-    inputs.append(stream.read(CHUNK, False))
-    inputs.append(stream.read(CHUNK, False))
-    inputs.append(stream.read(CHUNK, False))
-    inputs.append(stream.read(CHUNK, False))
-    inputs.append(stream.read(CHUNK, False))
-    inputs.append(stream.read(CHUNK, False))
-    inputs.append(stream.read(CHUNK, False))
-    inputs.append(stream.read(CHUNK, False))
-  
-    input = ''.join(inputs)
-  
-    np_data = np.frombuffer(input, dtype="int16")
-    #print(type(np_data))
-    #print(np_data.size)
-    #print(np_data.dtype)
-    #print(np_data.sum())
-    color = END_TEXT
-    max = np_data.max()
-    min = np_data.min()
+        night_text = small_font.render(night_str, True, self.text_color)
+        screen.blit(night_text, [700, 450])
 
-    if in_night:
-      max *= 2
+        for index, sound in enumerate(self.sound_logs):
+            graph_color = GREEN
+            width = 3
+            if sound > ALERT_THRESHOLD:
+                graph_color = WHITE
+                width = 30
+            elif sound > 200:
+                graph_color = YELLOW
+                width = 10
+            elif sound > 100:
+                graph_color = CYAN
+                width = 5
 
-    if max > ALERT_THRESHOLD:
-        color = RED_TEXT
-        alert_count += 1
-        subprocess.call(pixela_command, shell=True)
-    elif max > 200:
-        color = YELLOW_TEXT
-    elif max > 100:
-        color = CYAN_TEXT
-  
-    sound_logs = sound_logs + [max]
-    if len(sound_logs) > 200:
-        sound_logs.pop(0)
-    print(color + '{0}, {1}'.format(min, max) + END_TEXT)
+            sound_logs_len = len(self.sound_logs)
+            x = (sound_logs_len - index) * 3
+            pygame.draw.line(screen, graph_color, [x, 400], [x, 400 - sound / 2], width)
 
-    if alert_count_date != datetime.date.today():
-        alert_count = 0
-        alert_count_date = datetime.date.today()
 
+def main():
+    alert_count = 0
+    sound_logs =[]
+    carryOn = True
+    alert_count_date = datetime.date.today()
+
+    while carryOn:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                carryOn = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            pygame.display.set_mode(size)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_f:
+            pygame.display.set_mode(size, FULLSCREEN)
+
+        now = datetime.datetime.now()
+        in_night = is_night_time(now)
+
+        back_ground_color = BLACK
+        max_sound = 0
+        for sound in sound_logs:
+            if max_sound < sound:
+                max_sound = sound
+
+        text_color = WHITE
+        if max_sound > ALERT_THRESHOLD:
+            back_ground_color = BACK_GROUND_RED
+
+        if alert_count > 0:
+            text_color = TEXT_RED
+
+
+        screen_renderer = ScreenRenderer(screen, back_ground_color, alert_count, text_color, in_night, sound_logs)
+        screen_renderer.render()
+
+
+        #alert_count_text = font.render("ALERT", True, text_color)
+        #screen.blit(alert_count_text, [605, 100])
+
+
+      
+        pygame.display.flip()
+      
+        inputs = []
+        inputs.append(stream.read(CHUNK, False))
+        inputs.append(stream.read(CHUNK, False))
+        inputs.append(stream.read(CHUNK, False))
+        inputs.append(stream.read(CHUNK, False))
+        inputs.append(stream.read(CHUNK, False))
+        inputs.append(stream.read(CHUNK, False))
+        inputs.append(stream.read(CHUNK, False))
+        inputs.append(stream.read(CHUNK, False))
+      
+        input = ''.join(inputs)
+      
+        np_data = np.frombuffer(input, dtype="int16")
+        #print(type(np_data))
+        #print(np_data.size)
+        #print(np_data.dtype)
+        #print(np_data.sum())
+        color = END_TEXT
+        max = np_data.max()
+        min = np_data.min()
+
+        if in_night:
+          max *= 2
+
+        if max > ALERT_THRESHOLD:
+            color = RED_TEXT
+            alert_count += 1
+            subprocess.call(pixela_command, shell=True)
+        elif max > 200:
+            color = YELLOW_TEXT
+        elif max > 100:
+            color = CYAN_TEXT
+      
+        sound_logs = sound_logs + [max]
+        if len(sound_logs) > 200:
+            sound_logs.pop(0)
+        print(color + '{0}, {1}'.format(min, max) + END_TEXT)
+
+        if alert_count_date != datetime.date.today():
+            alert_count = 0
+            alert_count_date = datetime.date.today()
+
+main()
 
 pygame.quit()
 
